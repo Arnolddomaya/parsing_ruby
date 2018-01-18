cbe8513162d6e1a99358544b7ae0bf2633e7db6f
arnold-domaya <domaya_arnold@yahoo.fr>
2018-01-18 20:53:23 +0100

    exo2 ok bonus ok

--- projet_parsing/trader_obscur.rb
+++ projet_parsing/trader_obscur.rb
@@ -2,8 +2,9 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
+### Important d'être connecter à internet pour que tout marhce :)
 
-def trader()
+def parse_criptomonies()
   res = []
   page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
   #on recupère le tableau qui contient tout les cryptomonaies dans un tableau
@@ -18,5 +19,19 @@ def trader()
   res
 end
 
-#currencies-all > tbody:nth-child(2)
-puts trader()
+
+#boucle infinie qui relance la fonction tout les "t second"
+#pour les besoin de l'exo on met t = 15 s
+#on pourrait le mettre à 3600s = 1heure comme dans l'énoncé
+#appuyer sur ctrl +c pour arreter la boucle
+t = 15
+while true
+  parse_criptomonies().each do |crypto|
+    puts "nom : #{crypto[:name]},   cours = #{crypto[:price]}"  
+  end
+  #sleep arrête le programme pendant t second
+  sleep(t)
+  puts " =============MISE A JOUR====================="
+  puts " ============================================="
+  sleep(2)
+end
